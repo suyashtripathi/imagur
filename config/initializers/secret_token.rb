9152383e2c48ecefdfda1ef04f19c2a3e54bd4be
Suyash Tripathi <suyashtripathi@gmail.com>
2013-05-10 12:39:53 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imagur::Application.config.secret_token = '51225b8a4b2409c233d119383ae592572c56abfc48fc63c94a8387b081abc438d35c0837e12d47926db0fad9bb4a5c53966c804ae449656eb2d17bb66f373bd2'
